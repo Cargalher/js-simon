5458fd2794995841d6fdeac2f61fe33e09ee5b6a
Cargalher <cargalher@gmail.com>
2021-05-06 17:08:23 +0200

    creatin 5 random numbers

--- main.js
+++ main.js
@@ -3,3 +3,38 @@
 2.Da li parte un timer di 30 secondi.
 3.Dopo 30 secondi l'utente deve inserire, uno alla volta, i numeri che ha visto precedentemente, tramite il prompt().
 4.Dopo che sono stati inseriti i 5 numeri, il software dice quanti e quali dei numeri da indovinare sono stati individuati.*/
+
+
+
+function generateRandomNumber (){
+    var randomNumber = Math.floor(Math.random() * 20);
+    // console.log(randomNumber);
+}
+generateRandomNumber();
+
+var arr = [];
+for (var i=0; i<5; i++){
+// var randomNum = generateRandomNumber();
+arr.push(randomNumber)
+}
+
+console.log(arr);
+
+
+
+
+
+// function generateRandomNumber(min, max) { // min and max included 
+//     return Math.floor(Math.random()*(max - min + 1) + min);
+// }
+
+// generateRandomNumber(1, 100);
+// console.log(generateRandomNumber(1, 100));
+// alert ("Remember these numbers: " + Math.floor((Math.random() * 100) + 1));
+
+// var randomNumbers = [];
+// for (var i=0; i<5; i++){
+
+// }
+// var randomNum = generateFiveRandomNumbers(min, max);
+// console.log(randomNum);
